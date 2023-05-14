67595f9ca7d678e72017f6180234f3d50825d128
Bit <vlad10game@gmail.com>
2023-05-14 10:53:21 +0300

    Fix subscribe

--- timeTableVk/handlers/sub.py
+++ timeTableVk/handlers/sub.py
@@ -10,7 +10,7 @@ from tinydb import where
 from vkbottle.dispatch.rules.base import PayloadRule
 
 
-@labeler.message(PayloadRule({'action': 'unsubscribe'}))
+@labeler.message(PayloadRule({'action': 'subscribe'}))
 async def sub_labeler(message):
     await db.update({'allow_send': True}, where('peer_id') == message.peer_id)
     await message.answer(message='Вы успешно подписались на рассылку!')
