cdcfea27b11c188170f1361c2d34f94a4851507f
Bit <vlad10game@gmail.com>
2023-05-14 11:00:28 +0300

    Release new version 0.1.3
    Final Release
    Contains all bugfixes, own database, senders, basic usage. All set up.
    Changed logging level to INFO, changed version in pyproject.toml.
    
    Tested-by: Lonely Dark

--- timeTableVk/config.py
+++ timeTableVk/config.py
@@ -18,15 +18,16 @@ config.read(os.path.join(os.getcwd(), '.ini'))
 
 TOKEN = config['DEFAULT']['TOKEN']
 GROUP_ID = config['DEFAULT']['GROUP_ID']
+LEVEL = "INFO"
 
 api = API(token=TOKEN)
 labeler = BotLabeler()
 
 logger.remove()
 logger.add(os.path.join("logs", "log.log"), format="[{time:YYYY-MM-DD at HH:mm:ss}] [{level}]: {message}",
-           level="DEBUG", retention="10 days")
+           level=LEVEL, retention="10 days")
 logger.add(sys.stderr,
            format="<green>[{time:YYYY-MM-DD at HH:mm:ss}]</green> <cyan>[{level}]</cyan>: <level>{message}</level>",
-           level="DEBUG", colorize=True)
+           level=LEVEL, colorize=True)
 
 db = Database()
